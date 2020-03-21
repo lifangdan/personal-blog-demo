30a48aabff9747663d81544bffa03c52f7d4af4f
lifangdan <845154579@qq.com>
2020-03-21 21:36:22 +0800

    配置router和vuex

---
+++ src/views/pageA.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div>我是页面A</div>
+        <h1>{{$store.state.count}}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+
+</style>
